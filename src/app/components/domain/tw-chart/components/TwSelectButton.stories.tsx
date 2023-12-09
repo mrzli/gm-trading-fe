ac0859c64c512d17cdf72e2546d6f222c82faeae
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-09 23:46:15 +0100

    implement select button

--- src/app/components/domain/tw-chart/components/TwSelectButton.stories.tsx
+++ src/app/components/domain/tw-chart/components/TwSelectButton.stories.tsx
@@ -1,17 +1,54 @@
+import { useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
-import { TwSelectButton, TwSelectButtonProps } from './TwSelectButton';
+import {
+  TwSelectButton,
+  TwSelectButtonProps,
+  TwSelectOption,
+} from './TwSelectButton';
+import { range } from '@gmjs/array-create';
+import { PrettyDisplay } from '../../../shared/display/PrettyDisplay';
+import { decoratorPadding } from '../../../../../storybook';
+
+function createOption(id: number): TwSelectOption {
+  return {
+    label: `Option ${id}`,
+    value: `option-${id}`,
+  };
+}
+
+function createOptions(count: number): readonly TwSelectOption[] {
+  return range(1, count + 1).map((id) => createOption(id));
+}
+
+const OPTIONS: readonly TwSelectOption[] = createOptions(5);
 
 const STORY_META: Meta<TwSelectButtonProps> = {
   component: TwSelectButton,
   tags: ['autodocs'],
-  decorators: [],
+  decorators: [decoratorPadding()],
   argTypes: {},
-  args: {},
+  args: {
+    options: OPTIONS,
+  },
 };
 export default STORY_META;
 
 export const Primary: StoryObj<TwSelectButtonProps> = {
   render: (args: TwSelectButtonProps) => {
-    return <TwSelectButton {...args} />;
+    const { value: _ignore1, onValueChange: _ignore2, ...rest } = args;
+
+    const { options } = args;
+
+    const [value, setValue] = useState<string>(
+      options.length > 0 ? options[0].value : '',
+    );
+
+    return (
+      <div>
+        <TwSelectButton {...rest} value={value} onValueChange={setValue} />
+        <br />
+        <PrettyDisplay content={value} />
+      </div>
+    );
   },
 };
--- src/app/components/domain/tw-chart/components/TwSelectButton.tsx
+++ src/app/components/domain/tw-chart/components/TwSelectButton.tsx
@@ -1,8 +1,93 @@
-import React from 'react';
+/* eslint-disable jsx-a11y/click-events-have-key-events */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable jsx-a11y/no-static-element-interactions */
+import React, { useCallback, useState } from 'react';
+import {
+  FloatingFocusManager,
+  FloatingPortal,
+  flip,
+  offset,
+  useClick,
+  useDismiss,
+  useFloating,
+  useInteractions,
+} from '@floating-ui/react';
 
-export interface TwSelectButtonProps {
+export interface TwSelectOption {
+  readonly label: string;
+  readonly value: string;
 }
 
-export function TwSelectButton({}: TwSelectButtonProps): React.ReactElement {
-  return (<div>{'TwSelectButton'}</div>)
+export interface TwSelectButtonProps {
+  readonly options: readonly TwSelectOption[];
+  readonly value: string;
+  readonly onValueChange: (value: string) => void;
+}
+
+export function TwSelectButton({
+  options,
+  value,
+  onValueChange,
+}: TwSelectButtonProps): React.ReactElement {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const { refs, floatingStyles, context } = useFloating({
+    placement: 'bottom-start',
+    open: isOpen,
+    onOpenChange: setIsOpen,
+    middleware: [offset(2), flip({ padding: 10 })],
+  });
+
+  const click = useClick(context, { event: 'mousedown' });
+  const dismiss = useDismiss(context);
+
+  const { getReferenceProps, getFloatingProps, getItemProps } = useInteractions(
+    [dismiss, click],
+  );
+
+  const handleSelect = useCallback(
+    (v: string, _index: number) => {
+      setIsOpen(false);
+      onValueChange(v);
+    },
+    [onValueChange, setIsOpen],
+  );
+
+  return (
+    <div>
+      <div
+        ref={refs.setReference}
+        className='px-1 text-sm border rounded border-slate-400 bg-slate-100 cursor-pointer outline-none inline-flex items-center justify-center min-h-[24px] min-w-[24px]'
+        {...getReferenceProps()}
+      >
+        {value}
+      </div>
+      {isOpen && (
+        <FloatingPortal>
+          <FloatingFocusManager context={context} modal={false}>
+            <div
+              ref={refs.setFloating}
+              style={floatingStyles}
+              className='text-sm border rounded border-slate-400 bg-slate-100 overflow-y-auto bg-white outline-none'
+              {...getFloatingProps()}
+            >
+              {options.map((o, i) => (
+                <div
+                  key={o.value}
+                  className={'cursor-pointer px-1 hover:bg-slate-200'}
+                  onClick={() => {
+                    handleSelect(o.value, i);
+                  }}
+                  {...getItemProps()}
+                >
+                  {o.label}
+                </div>
+              ))}
+            </div>
+          </FloatingFocusManager>
+        </FloatingPortal>
+      )}
+    </div>
+  );
 }
