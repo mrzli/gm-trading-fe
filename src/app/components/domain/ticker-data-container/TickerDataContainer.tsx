9264f61416c7f873c113cbd43257a8c927422303
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-10 18:24:01 +0100

    refactor ticker chart container

---
+++ src/app/components/domain/ticker-data-container/TickerDataContainer.tsx
@@ -0,0 +1,86 @@
+import React, { useEffect, useMemo, useState } from 'react';
+import { UTCTimestamp } from 'lightweight-charts';
+import { TickerDataRow } from '../../../types';
+import { LoadingDisplay } from '../../shared/display/LoadingDisplay';
+import { TwChart } from '../tw-chart/TwChart';
+import { parseFloatOrThrow, parseIntegerOrThrow } from '@gmjs/number-util';
+import { TwChartResolution, TwChartSettings } from '../tw-chart/types';
+import { TwChartToolbar } from '../tw-chart/components/TwChartToolbar';
+import { Instrument } from '@gmjs/gm-trading-shared';
+
+export interface TickerDataContainerProps {
+  readonly allInstruments: readonly Instrument[];
+  readonly isLoadingData: boolean;
+  readonly rawData: readonly string[] | undefined;
+  readonly onRequestData: (name: string, resolution: TwChartResolution) => void;
+}
+
+export function TickerDataContainer({
+  allInstruments,
+  isLoadingData,
+  rawData,
+  onRequestData,
+}: TickerDataContainerProps): React.ReactElement {
+  const instrumentNames = useMemo(() => {
+    return allInstruments?.map((instrument) => instrument.name) ?? [];
+  }, [allInstruments]);
+
+  const [settings, setSettings] = useState<TwChartSettings>({
+    instrumentName: allInstruments[0].name,
+    resolution: '5m',
+  });
+
+  const { instrumentName, resolution } = settings;
+
+  useEffect(() => {
+    onRequestData(instrumentName, resolution);
+  }, [onRequestData, instrumentName, resolution]);
+
+  const instrument = useMemo(() => {
+    return allInstruments.find(
+      (instrument) => instrument.name === instrumentName,
+    );
+  }, [allInstruments, instrumentName]);
+
+  const finalData = useMemo(() => toTickerDataRows(rawData ?? []), [rawData]);
+
+  if (!instrument) {
+    return <div>Instrument not found.</div>;
+  }
+
+  const dataChartElement =
+    !isLoadingData && finalData.length > 0 ? (
+      <TwChart precision={instrument.precision} data={finalData} />
+    ) : isLoadingData ? (
+      <LoadingDisplay />
+    ) : (
+      <div>Please load data...</div>
+    );
+
+  return (
+    <div className='h-screen flex flex-col gap-4 p-4'>
+      <TwChartToolbar
+        instrumentNames={instrumentNames}
+        settings={settings}
+        onSettingsChange={setSettings}
+      />
+      <div className='flex-1'>{dataChartElement}</div>
+    </div>
+  );
+}
+
+function toTickerDataRows(lines: readonly string[]): TickerDataRow[] {
+  return lines.map((element) => toTickerDataRow(element));
+}
+
+function toTickerDataRow(line: string): TickerDataRow {
+  const [timestamp, _date, open, high, low, close] = line.split(',');
+
+  return {
+    time: parseIntegerOrThrow(timestamp) as UTCTimestamp,
+    open: parseFloatOrThrow(open),
+    high: parseFloatOrThrow(high),
+    low: parseFloatOrThrow(low),
+    close: parseFloatOrThrow(close),
+  };
+}
--- src/app/screens/TickerDataScreen.tsx
+++ src/app/screens/TickerDataScreen.tsx
@@ -1,6 +1,43 @@
-import React from 'react';
-import { TickerDataContainer } from '../components/domain/TickerDataContainer';
+import React, { useCallback, useEffect } from 'react';
+import { TickerDataContainer } from '../components/domain/ticker-data-container/TickerDataContainer';
+import { useStoreInstrument, useStoreTickerData } from '../../store';
+import { LoadingDisplay } from '../components/shared/display/LoadingDisplay';
+import { TwChartResolution } from '../components/domain/tw-chart/types';
 
 export function TickerDataScreen(): React.ReactElement {
-  return <TickerDataContainer />;
+  const { isLoadingAllInstruments, allInstruments, getAllInstruments } =
+    useStoreInstrument();
+
+  const { isLoadingTickerData, tickerData, getTickerData } =
+    useStoreTickerData();
+
+  useEffect(() => {
+    getAllInstruments();
+  }, [getAllInstruments]);
+
+  const handleRequestData = useCallback(
+    (name: string, _resolution: TwChartResolution) => {
+      getTickerData({
+        name,
+        resolution: 'minute',
+        date: undefined,
+      });
+    },
+    [getTickerData],
+  );
+
+  if (isLoadingAllInstruments || !allInstruments) {
+    return <LoadingDisplay />;
+  } else if (allInstruments.length === 0) {
+    return <div>No instruments found.</div>;
+  }
+
+  return (
+    <TickerDataContainer
+      allInstruments={allInstruments}
+      isLoadingData={isLoadingTickerData}
+      rawData={tickerData?.data}
+      onRequestData={handleRequestData}
+    />
+  );
 }
