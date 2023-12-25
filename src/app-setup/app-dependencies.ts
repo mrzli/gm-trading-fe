abe57ff0af3fdb46c94da636425571abae9e3bdd
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-25 22:59:04 +0100

    add access to browser storage

--- src/app-setup/app-dependencies.ts
+++ src/app-setup/app-dependencies.ts
@@ -1,16 +1,32 @@
+import {
+  CookieWrapper,
+  createCookieWrapper,
+  createLocalStorageWrapper,
+  createSessionStorageWrapper,
+  StorageWrapper,
+} from '@gmjs/browser-storage';
 import { AppConfig } from './app-config';
 import { createServer } from './server';
 import { AppApi, createAppApi } from '../api';
 
 export interface AppDependencies {
   readonly api: AppApi;
+  readonly cookie: CookieWrapper<string>;
+  readonly localStorage: StorageWrapper<string>;
+  readonly sessionStorage: StorageWrapper<string>;
 }
 
 export function createAppDependencies(config: AppConfig): AppDependencies {
   const server = createServer(config);
   const api = createAppApi(server);
+  const cookie = createCookieWrapper();
+  const localStorage = createLocalStorageWrapper();
+  const sessionStorage = createSessionStorageWrapper();
 
   return {
     api,
+    cookie,
+    localStorage,
+    sessionStorage,
   };
 }
