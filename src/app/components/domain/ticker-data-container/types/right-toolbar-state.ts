dac92a4c091c28ff156e8380cde5395055726a4a
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-26 17:13:58 +0100

    remove some unused code

--- src/app/components/domain/ticker-data-container/types/right-toolbar-state.ts
+++ src/app/components/domain/ticker-data-container/types/right-toolbar-state.ts
@@ -1,3 +1,3 @@
-export const TYPES_OF_RIGHT_TOOLBAR_STATE = ['none', 'trade'] as const;
+export const TYPES_OF_RIGHT_TOOLBAR_STATE = ['trade'] as const;
 
 export type RightToolbarState = (typeof TYPES_OF_RIGHT_TOOLBAR_STATE)[number];
