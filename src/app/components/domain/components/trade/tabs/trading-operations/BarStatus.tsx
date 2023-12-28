7ff880a2712801561f53c877935c314b307025c7
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-28 14:12:38 +0100

    display time and open price for curretn bar in trade tab

--- src/app/components/domain/components/trade/tabs/trading-operations/BarStatus.tsx
+++ src/app/components/domain/components/trade/tabs/trading-operations/BarStatus.tsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useMemo } from 'react';
 import { BarReplayPosition } from '../../../../types';
 import { TradingDataAndInputs } from '../../types';
 import { BarReplay } from './bar-replay/BarReplay';
+import { DateValueDisplay, DecimalValueDisplay } from '../../../shared';
+import { getOhlc } from '../../util/process/ohlc';
 
 export interface BarStatusProps {
   readonly dataAndInputs: TradingDataAndInputs;
@@ -12,16 +14,43 @@ export function BarStatus({
   dataAndInputs,
   onReplayPositionChange,
 }: BarStatusProps): React.ReactElement {
-  const { fullData, replayPosition } = dataAndInputs;
+  const { settings, fullData, replayPosition, barData, barIndex, inputs } =
+    dataAndInputs;
+  const { timezone } = settings;
   const { subBars } = fullData;
+  const { params } = inputs;
+  const { priceDecimals, spread } = params;
+
+  const bar = barData[barIndex];
+  const time = bar.time;
+
+  const barBuy = useMemo(() => getOhlc(bar, true, spread), [bar, spread]);
+  const barSell = useMemo(() => getOhlc(bar, false, spread), [bar, spread]);
 
   return (
-    <div className='flex flex-col'>
+    <div className='flex flex-col gap-2'>
       <BarReplay
         subBars={subBars}
         replayPosition={replayPosition}
         onReplayPositionChange={onReplayPositionChange}
       />
+      <div className='flex flex-row gap-2'>
+        <DateValueDisplay
+          label={'Next Bar Time'}
+          value={time}
+          timezone={timezone}
+        />
+        <DecimalValueDisplay
+          label={'Open Buy'}
+          value={barBuy.o}
+          precision={priceDecimals}
+        />
+        <DecimalValueDisplay
+          label={'Open Sell'}
+          value={barSell.o}
+          precision={priceDecimals}
+        />
+      </div>
     </div>
   );
 }
