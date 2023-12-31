f04482050ea672f5a1dff441d0591f784682bb9b
Goran Mržljak <goran.mrzljak@gmail.com>
2023-12-31 17:26:35 +0100

    add timezone to bar replay go-to
    This can be used to navigate bar replay based on different timezone that what is selected for the chart.

--- src/app/components/domain/components/trade/tabs/trading-operations/bar-replay/BarReplayGoTo.tsx
+++ src/app/components/domain/components/trade/tabs/trading-operations/bar-replay/BarReplayGoTo.tsx
@@ -1,16 +1,22 @@
 import React, { useCallback, useMemo, useState } from 'react';
 import { dateIsoToUnixSeconds } from '../../../../../../../util';
-import { TextInput } from '../../../../../../shared';
+import {
+  SelectButton,
+  SelectOption,
+  TextInput,
+} from '../../../../../../shared';
 import {
   SCHEMA_DATE_INPUT,
   SCHEMA_DATE_FOR_INPUT,
   dateInputToIso,
   binarySearch,
+  toSimpleSelectOption,
 } from '../../../../../util';
 import {
   ChartTimezone,
   GroupedBars,
   BarReplayPosition,
+  TYPES_OF_CHART_TIMEZONES,
 } from '../../../../../types';
 
 export interface BarReplayGoToProps {
@@ -37,6 +43,13 @@ export function BarReplayGoTo({
     [goToInput, subBars],
   );
 
+  const [navigationTimezone, setNavigationTimezone] =
+    useState<TimezoneOrCurrent>('current-tz');
+
+  const finalTimezone = useMemo<ChartTimezone>(() => {
+    return navigationTimezone === 'current-tz' ? timezone : navigationTimezone;
+  }, [navigationTimezone, timezone]);
+
   const handleGoToInputKeyDown = useCallback(
     (event: React.KeyboardEvent<HTMLInputElement>) => {
       if (event.key === 'Enter') {
@@ -44,7 +57,10 @@ export function BarReplayGoTo({
           return;
         }
 
-        const time = dateIsoToUnixSeconds(dateInputToIso(goToInput), timezone);
+        const time = dateIsoToUnixSeconds(
+          dateInputToIso(goToInput),
+          finalTimezone,
+        );
         const barIndex = binarySearch(subBars, time, (item) => item[0].time);
         if (barIndex === replayPosition.barIndex) {
           return;
@@ -54,12 +70,12 @@ export function BarReplayGoTo({
       }
     },
     [
+      finalTimezone,
       goToInput,
       isGoToValid,
       onReplayPositionChange,
       replayPosition.barIndex,
       subBars,
-      timezone,
     ],
   );
 
@@ -73,6 +89,23 @@ export function BarReplayGoTo({
         error={!isGoToInputValid}
         width={160}
       />
+      <SelectButton<TimezoneOrCurrent, false>
+        options={TIMEZONE_OPTIONS}
+        value={navigationTimezone}
+        onValueChange={setNavigationTimezone}
+        selectionWidth={128}
+        selectItemWidth={128}
+      />
     </div>
   );
 }
+
+const TIMEZONE_OR_CURRENT = [
+  'current-tz',
+  ...TYPES_OF_CHART_TIMEZONES,
+] as const;
+
+type TimezoneOrCurrent = (typeof TIMEZONE_OR_CURRENT)[number];
+
+const TIMEZONE_OPTIONS: readonly SelectOption<TimezoneOrCurrent>[] =
+  TIMEZONE_OR_CURRENT.map((timezone) => toSimpleSelectOption(timezone));
