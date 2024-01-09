9d36dc5325a7eb4688f72c36f82409eedee60387
Goran Mržljak <goran.mrzljak@gmail.com>
2024-01-09 18:33:05 +0100

    update comment

--- src/app/components/domain/components/tw-chart/util/date.ts
+++ src/app/components/domain/components/tw-chart/util/date.ts
@@ -16,9 +16,10 @@ import {
 //   - if I put the timezone to Europe/Berlin, the chart would need to display 13:00
 //   - to fake that, we need a timestamp of 1704114000, or 2024-01-01T13:00:00.000Z
 // process is:
-//   - convert timestamp to luxon DateTime, to the zone Europe/Berlin
+//   - convert timestamp to DateObjectTz,
+//     which will represent that same time point in the zone Europe/Berlin
 //   - it will have a timestamp 1704110400, and time of 2024-01-01T13:00:00.000+01:00
-//   - then we set the zone to UTC, and but keep the local time (meaning hour 13)
+//   - then we (re-)set the zone to UTC, and but keep the local time (meaning hour 13)
 //   - the resulting time is 2024-01-01T13:00:00.000Z, and the timestamp is 1704114000
 export function utcToTzTimestamp(
   timestamp: number,
